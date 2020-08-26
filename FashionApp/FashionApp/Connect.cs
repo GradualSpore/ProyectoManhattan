d40df396f7b0842de0b2b538d3049511cd3689ab
AbelGalvan <37642705+AbelGalvan@users.noreply.github.com>
2020-08-25 21:28:51 -0600

    Conexion

---
+++ FashionApp/FashionApp/Connect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+
+namespace FashionApp
+{
+    class Connect
+    {
+        string usr;
+        string pass;
+
+        public Connect(string user, string password)
+        {
+            usr = user;
+            pass = password;
+        }
+
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+
+        public bool TryConnection(out string Error)
+        {
+            builder.Server = "201.146.25.42";
+            builder.Port = 3306;
+            builder.Database = "basepruebaapp";
+            builder.UserID = usr;
+            builder.Password = pass;
+
+            try
+            {
+                MySqlConnection ms = new MySqlConnection(builder.ToString());
+                ms.Open();
+                Error = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Error = ex.ToString();
+                return false;
+            }
+        }
+
+    }
+}
